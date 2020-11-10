a93f1cfd79c3d662336b84a10a70bba9962288f2
Christoph Vögele <christoph.voegele@students.fhnw.ch>
2020-11-10 14:34:22 +0100

    add smooth zoom

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -7,27 +7,41 @@ public class CameraMovement : MonoBehaviour
 {
     Vector3 startPosition = new Vector3(10, 2, 0);
 
+    public Camera mainCamera;
+    public float minDistance;
+
     void Start()
     {
-        transform.position = startPosition;
+        startPosition = mainCamera.transform.position;
     }
-
+    
+    private Vector3 moveTo;
+    
     void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
 
             if (Physics.Raycast(ray, out hit))
             {
                 if(hit.rigidbody != null)
                 {
                     Vector3 temp = hit.rigidbody.position;
-                    temp.x = 5;
-                    transform.position = temp;
+                    Debug.Log($"{temp}");
+                    var direction = (transform.position - temp).normalized;
+                    direction.Scale(new Vector3(minDistance, minDistance, minDistance));
+                    moveTo = temp + direction;
                 }
             }
+        }
+
+        if (moveTo != Vector3.zero && moveTo != transform.position)
+        {
+            //Debug.Log($"{moveTo}");
+            var moveThisFrame = (moveTo - transform.position) * Time.deltaTime;
+            transform.position += moveThisFrame;
         }
     }
 }
