2c664f1df1fbb69672bc9d0e26372e907dc25c75
Christoph Vögele <christoph.voegele@students.fhnw.ch>
2020-11-11 17:54:06 +0100

    fix not being able to select objects upon loading the game fresh

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -14,6 +14,7 @@ public class CameraMovement : MonoBehaviour
     void Start()
     {
         startPosition = transform.position;
+        hitObjectPosition = startPosition;
     }
 
     private Vector3 moveTo;
