07c6857a842373071f92dd2732ec98406c14b9c4
Christoph Voegele <christoph228@gmail.com>
2020-12-15 20:20:24 +0100

    add outline material change

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private Vector3 startPosition;
+    
+    public float minDistance;
+    public float movementTime;
+
+    private float tick;
+    private Vector3 destination;
+    private Vector3 hitObjectPosition;
+    private Vector3 originalViewDirection;
+    private Vector3 movementStartPosition;
+
+    public float rotationSpeed;
+    void Start()
+    {
+        var position = transform.position;
+        startPosition = position;
+        hitObjectPosition = startPosition;
+        originalViewDirection = Vector3.zero - position; 
+    }
+
+
+
+    void Update()
+    {
+       
+       
+    }
+
+    public void ResetDestination()
+    {
+        SetDestination(startPosition);
+    }
+
+    public void SetDestination(Vector3 hitObjectPosition) 
+    {
+        var offset = (startPosition - hitObjectPosition).normalized;
+        offset.Scale(new Vector3(minDistance, minDistance, minDistance));
+        destination = hitObjectPosition + offset;
+        movementStartPosition = transform.position;
+        tick = 0f;
+        StartCoroutine(nameof(MoveAndRotate));
+    }
+
+    IEnumerator MoveAndRotate()
+    {
+        while (tick < movementTime)
+        {
+            Vector3 targetDirection = destination - transform.position;
+            //rotate Camera
+            if (destination == startPosition)
+            {
+                targetDirection = originalViewDirection;
+            }
+
+            float singleRotationStep = rotationSpeed * Time.deltaTime;
+            var newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleRotationStep, 0f);
+            transform.rotation = Quaternion.LookRotation(newDirection);
+
+            //move camera
+            tick += Time.deltaTime;
+            var moveThisFrame = Vector3.Lerp(movementStartPosition, destination, tick / movementTime);
+            transform.position = moveThisFrame;
+            yield return null;
+        }
+    }
+    
+}
---
+++ Assets/Scripts/Selector.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selector : MonoBehaviour
+{
+
+    private CameraMovement cameraMovement;
+    private GameObject selection;
+    public Material selectionMaterial;
+    public Material oldMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraMovement = GetComponentInChildren<CameraMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction, Color.magenta, 20);
+            if (Physics.Raycast(ray, out hit, LayerMask.GetMask("Hittable")))
+            {
+                if (hit.rigidbody != null)
+                {
+                    //if nothing selected prior
+                    if (selection == null)
+                    {
+                        selection = hit.transform.gameObject;
+                        turnOnSelectionMaterial();
+                    }
+                    //if already something selected
+                    else
+                    {
+                        //press same object again
+                        if (selection == hit.rigidbody.gameObject)
+                        {
+
+                            var position = hit.rigidbody.position;
+                            Vector3 newHitPosition = position;
+
+                            cameraMovement.SetDestination(newHitPosition);
+
+                        }
+                        else
+                        //press something else
+                        {
+                            turnOffSelectionMaterial();
+                            selection = hit.transform.gameObject;
+                            turnOnSelectionMaterial();
+                        }
+
+                    }
+                }
+            }
+            //hit nothing
+            else
+            {
+                turnOffSelectionMaterial();
+                selection = null;
+                cameraMovement.ResetDestination();
+            }
+
+        }
+
+    }
+
+    private void turnOnSelectionMaterial()
+    {
+
+        var renderer = selection.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            oldMaterial = renderer.material;
+            renderer.material = selectionMaterial;
+        }
+
+    }
+
+    private void turnOffSelectionMaterial()
+    {
+        var renderer = selection.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.material = oldMaterial;
+            oldMaterial = null;
+        }
+    }
+}
